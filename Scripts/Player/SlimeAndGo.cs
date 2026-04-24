ca56d374af5ab4aa6f70dc29bac79741f8f7dd3d
moodude <moodude@gmx.de>
2026-04-24 16:35:04 +0200

    corrected collision area and collision body bool

--- Scripts/Player/SlimeAndGo.cs
+++ Scripts/Player/SlimeAndGo.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 
 
 
+
 public partial class SlimeAndGo : Node
 {
 #region Struct
@@ -69,12 +70,12 @@ public partial class SlimeAndGo : Node
 	private void ApplyVector(Vector2 validVector, IMovementEntity callingEntity)
 	{
 		var node = callingEntity as Node2D;
-		GD.Print($"before moving node.GlobalPosition is : {node.GlobalPosition}");
-        GD.Print($"I move by : {validVector}");
+		//GD.Print($"before moving node.GlobalPosition is : {node.GlobalPosition}");
+        //GD.Print($"I move by : {validVector}");
        if (node is not null)
         {
             node.GlobalPosition += validVector;
-			GD.Print($"after moving node.GlobalPosition is : {node.GlobalPosition}");
+			//GD.Print($"after moving node.GlobalPosition is : {node.GlobalPosition}");
         }
 	}
 
@@ -95,7 +96,7 @@ public partial class SlimeAndGo : Node
 private Vector2 CreateInitialVector(PhysicsShapeQueryParameters2D query, IMovementEntity entity,  float delta)
 	{
 		//get input Vector NORMALIZED! speed and delta to create initialMotion
-		Vector2 initialMotion = query.Motion * entity.SpeedMulti * delta;
+		Vector2 initialMotion = query.Motion.Normalized() * entity.SpeedMulti * delta;
 
 		return initialMotion;
 	}
@@ -103,15 +104,15 @@ private Vector2 CreateInitialVector(PhysicsShapeQueryParameters2D query, IMoveme
 private PhysicsShapeQueryParameters2D CreateQuery (IMovementEntity callingEntity)
 	{
 			PhysicsShapeQueryParameters2D originalQuery = new PhysicsShapeQueryParameters2D();
-			originalQuery.CollideWithAreas = false; //if true, query will take Area2D into account
-			originalQuery.CollideWithBodies = true; //if true, query will take PhysicsBody2D into account
+			originalQuery.CollideWithAreas = true; //if true, query will take Area2D into account
+			originalQuery.CollideWithBodies = false; //if true, query will take PhysicsBody2D into account
 			//originalQuery.CollisionMask = ?
 			originalQuery.Margin = 0.0f;
 			originalQuery.Motion = callingEntity.Velocity; // here check what velocity does!
 			originalQuery.Shape = callingEntity.MyShape.Shape;
 			originalQuery.Transform = callingEntity.MyShape.GlobalTransform;
 
-			// TODO: create debug tests for query output (static, small motion, edge cases)
+			
 			// verify what PhysicsShapeQueryParameters2D actually represents in space
 			//static and small motion test = values of variables are fine. no unexpected behavior at early point 22.04
 
@@ -121,38 +122,81 @@ private PhysicsShapeQueryParameters2D CreateQuery (IMovementEntity callingEntity
 private SweepData ShapeSweeper(PhysicsShapeQueryParameters2D originalQuery, Node2D node)
 	{
 		var spaceState2D = node.GetWorld2D().DirectSpaceState;
+		PhysicsShapeQueryParameters2D sweeperQuery = new();
+		///resting position overlap check
 		Dictionary restResultAtStart = spaceState2D.GetRestInfo(originalQuery);
-		if(restResultAtStart.Count != 0)
+		if(restResultAtStart.Count != 0) //might throw null reference or crash if null <- test if nullcheck needed? 
 		{
-		GD.Print($@"At resting Point
-		Collision Point is : {restResultAtStart["point"]}
-		Querys Shape Data is : {restResultAtStart["shape"]}
-		Surface Normal is : {restResultAtStart["normal"]}
-		Collider ID is : {restResultAtStart["collider_id"]}
+	//		GD.Print($@"At resting is a collision:
+	//	Collision Point is : {restResultAtStart["point"]}
+	//	Querys Shape Data is : {restResultAtStart["shape"]}
+	//	Surface Normal is : {restResultAtStart["normal"]}
+	//	Collider ID is : {restResultAtStart["collider_id"]}
+	//	 ");
+	//		ulong colliderID = (ulong)restResultAtStart["collider_id"];
+	//		var collider = InstanceFromId(colliderID);
+	//		GD.Print(collider);
 
-		 ");
-		 ulong colliderID = (ulong)restResultAtStart["collider_id"];
-		 var collider = InstanceFromId(colliderID);
-		GD.Print(collider);
+			return new SweepData
+			{
+				collisionPoint = (Vector2)restResultAtStart["point"],	
+				safeMotionMargin = 0,
+				Normal = (Vector2)restResultAtStart["normal"]
+			};
+		}
+		///end resting position overlap check
+		
+		///castMotion check for given query.motion
+		float[] motionCast = spaceState2D.CastMotion(originalQuery);
+		GD.Print($"Motion Cast Ergebnis = {motionCast[0]}");
+		var startPosition = originalQuery.Transform.Origin;
+		GD.Print($"startPosition : {startPosition}");
+		var endPosition = startPosition + originalQuery.Motion;
+		GD.Print($"end position without collision :{endPosition}");
+		if (Mathf.IsEqualApprox(motionCast[0],1f)) //100% of motion 
+		{
+			GD.Print("Motion Cast detected no collision");
+			return new SweepData
+			{
+				collisionPoint = null,
+				safeMotionMargin = 1,
+				Normal = null
+			};
 		}
 		
-		//resting postiton overlap check
-		//castMotion check for given query.motion
 		//castMotion from new() position, motion
-		//resting position overlap check
+			GD.Print("Motion Cast detected a collision");
+			endPosition = startPosition + motionCast[0] * originalQuery.Motion;
+			GD.Print($"end position with collision :{endPosition}");
+			Vector2 endCollisionPoint = startPosition + (motionCast[0] + float.Epsilon) * originalQuery.Motion;
+			GD.Print($"end Collision Point : {endCollisionPoint}");
+			sweeperQuery.Shape = originalQuery.Shape;
+        	sweeperQuery.Transform = new Transform2D(0, endCollisionPoint);
+        	sweeperQuery.Motion = Vector2.Zero;
+        	sweeperQuery.CollideWithAreas = originalQuery.CollideWithAreas;
+        	sweeperQuery.CollideWithBodies = originalQuery.CollideWithBodies;
+			//resting position overlap check at the end
+			Dictionary restResultAtEnd = spaceState2D.GetRestInfo(sweeperQuery);
+			if(restResultAtEnd.Count != 0)
+			{
 
-
-
-
-
-
+				return new SweepData
+				{
+					collisionPoint = (Vector2)restResultAtEnd["point"],	
+					safeMotionMargin = 0,
+					Normal = (Vector2)restResultAtEnd["normal"]
+				};
+			}
+			else
+			{
+				return new SweepData
+				{
+					collisionPoint = (Vector2)restResultAtEnd["point"],
+					safeMotionMargin = motionCast[0],
+					Normal = (Vector2)restResultAtEnd["normal"]
+				};
+			}
 		
-		return new SweepData
-		{
-			collisionPoint = Vector2.Zero,
-       		safeMotionMargin = 0.0f,
-        	Normal = Vector2.Zero,
-		};
 	}
 #endregion
 }
